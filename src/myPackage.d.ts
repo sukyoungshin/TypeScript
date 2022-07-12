3f9a00aee3721918babbf734942950987782df64
suKyoung <sukyoung_shin@kolon.com>
2022-07-12 12:51:52 +0900

    fix: 에러픽스

--- src/myPackage.d.ts
+++ src/myPackage.d.ts
@@ -1,3 +1,4 @@
+import './myPackage';
 // d.ts의 역할
 // - TS에게 타입에 대해서 설명한다.
 // - 패키지가 어떻게 생겼는지, 어떤 함수랑 다른 것들이 있는지를 설명해주는 파일
@@ -5,9 +6,16 @@ interface Config {
   url: string;
 }
 
-export declare function init(config: Config): boolean;
-export declare function exit(code: number): number;
+// Work
+// export declare function init(config: Config): boolean;
+// export declare function exit(code: number): number;
 
+declare module "./myPackage" {
+  function init(config: Config): boolean;
+  function exit(code: number): number;
+}
+
+// Error
 // declare module "myPackage" {
 //   function init(config: Config): boolean;
 //   function exit(code: number): number;
