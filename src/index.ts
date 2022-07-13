3a2f1106279a32ba6fe9801b74903779469a5680
SuKyoung Shin <sukyoung.dev@gmail.com>
2022-07-13 22:00:36 +0900

    5.6 DefinitelyTyped 진행 (TS에러 fix 2307, 2355)

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,13 @@ class Block implements BlockShape {
     this.hash = Block.calculateHash(prevHash, height, data);
   }
 
-  static calculateHash(prevHash: string, height: number, data: string): string {
+  static calculateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height}${data}`;
-
+    return (
+      crypto
+        .createHash("sha256")
+        .update(toHash)
+        .digest("hex")
+      );
   }
 }
